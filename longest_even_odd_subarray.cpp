c44b31dc5e866bd232d3d543da1c75f3f5178c0d
Saksham <112968510+Saksham13112002@users.noreply.github.com>
2022-09-08 23:53:06 +0530

    Create longest_even_odd_subarray.cpp

---
+++ longest_even_odd_subarray.cpp
@@ -0,0 +1,41 @@
+/*  Time Complexity -> O(n)  */
+
+#include<iostream>
+using namespace std;
+
+void input(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+}
+
+int solve(int arr[], int n)
+{
+    int ans = 1;
+
+    for(int i=0;i<n-1;i++)
+    {
+        if((arr[i]%2==0 && arr[i+1]%2!=0) || (arr[i]%2!=0 && arr[i+1]%2==0))
+            ans++;
+        else
+            ans = 1;
+    }
+    return ans;
+}
+
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"Enter the elements in the array\n";
+    input(arr, n);
+
+    cout<<"The longest even odd subarray is: "<<solve(arr, n)<<endl;
+    
+    return 0;
+}
